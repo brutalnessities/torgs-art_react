b9a03ad21200d48ec16dd537c3196d29e0d6615f
brutalnessities <brutalnessities@gmail.com>
2026-04-11 04:16:53 -0600

    make the gallery look nicer and add a dropdown for the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import "./App.css";
 import Gallery from "./components/gallary";
+import { useState } from "react";
+
+const dropdownItems = [
+  { name: "Home", link: "/" },
+  { name: "Gallery", link: "/gallery" },
+  { name: "About", link: "/about" },
+  { name: "Contact", link: "/contact" },
+];
+
+function DropdownMenu() {
+  return (
+    <div className="dropdown-menu">
+      {dropdownItems.map((item) => (
+        <div>
+          <a key={item.name} href={item.link} className="dropdown-item">
+            {item.name}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  function toggleDropdown() {
+    setIsDropdownOpen(!isDropdownOpen);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        Torgs Art
+      <header>
+        <div className="header-content">
+          <img src="/artsy-logo.png" className="logo" alt="logo" />
+          <h3>Torgs Art</h3>
+          <img src="/menu.svg" className="menu" onClick={toggleDropdown} />
+        </div>
+        {isDropdownOpen && <DropdownMenu />}
       </header>
       <main>
         <Gallery />
