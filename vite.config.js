24fefae90576804a95f4cd55533d60027ab9e92a
brutalnessities <brutalnessities@gmail.com>
2026-05-14 21:55:14 -0600

    redeploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "build", // CRA's default build output
+    outDir: "build", // CRA's default build outputs
   },
   resolve: {
     alias: {
